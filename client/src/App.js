b83d010c9596bb2a3a23697ea199f8a58d61144b
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-15 21:56:55 -0700

    t

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
+import Login from './components/Login';
+import Register from './components/Register';
+import Profile from './components/Profile';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -19,11 +27,12 @@ class App extends Component {
     this.callAPI();
   }
 
-
   render(){
     return (
-    <div className="App">
-      <header className="App-header">
+      <Router>
+      <div className="App">
+        <Navbar />
+        <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           VVS <code>src/App.js</code> and save to reload.
@@ -37,7 +46,14 @@ class App extends Component {
         </a>
       </header>
       <p className="App-intro">{this.state.apiResponse}</p>
-    </div>
+        <Route exact path="/" component={landing} />
+        <div className="container">
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={Profile} />
+        </div>
+      </div>
+      </Router>  
     );
   }
   
