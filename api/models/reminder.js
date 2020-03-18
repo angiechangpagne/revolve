59f97b60e62d2b9f9f51f70fddb3e2d7b2369ea2
angiechangpagne@gmail.com <angiechangpagne@gmail.com>
2020-03-18 15:54:01 -0700

    redhat

--- api/models/reminder.js
+++ api/models/reminder.js
@@ -43,5 +43,59 @@ reminderSchema.methods.requiresNotification = (date) => {
 };
 
 reminderSchema.statics.sendNotifications = (callback) => {
-  
+  const searchDate = new Date();
+  console.log('searchDate : '+searchDate);
+  Reminder
+    .find()
+    .then((reminders) => {
+      reminders = reminders.filter((reminder) => {
+        return reminder.requiresNotification(searchDate);
+      });
+      if(reminders.length > 0){
+        console.log("I found a Reminder!");
+        console.log(reminders);
+        sendNotifications(reminders);
+      } else {
+        console.log("duration");
+        console.log(Math.round(moment.duration(moment(this.time).utc()
+                        .diff(moment(searchDate).utc())
+                        ).asMinutes()));
+      }
+    });
+
+    const sendNotifications = (reminders) => {
+      
+      
+      const client = new Twilio(config.twilioAccount$id, config.twilioAuthToken);
+      reminders.forEach((reminder) => {
+        const message = {
+          to: `+1${reminder.reminderNumber}`,
+          from: config.twilioPhoneNumber,
+          body: `Hi! Just a reminder to ${reminder.reminderName} is up in ${reminder.notificationLabel}!`,
+        };
+
+        client.reminders.create(reminder, (err, res) => {
+          if(err) {
+            console.log(err);
+          } else {
+            let phoneNumber = reminder.reminderNumber;
+            console.log(`Reminder sent to ${phoneNumber}`);
+          }
+        });
+      });
+      if(callback) {
+        callback.call();
+      }
+    }
+};
+
+reminderSchema.statics.updateNotifications = () => {
+  console.log("I am finding an expired reminder");
+  Reminder
+    .updateMany( { "time" : { $lte : new Date() }, "notification" : { $gt : 0 } }, { $set : { "notification" : 0} } )
+    .then(dbRmdr => console.log(dbRmdr))
+    .catch(err => console.log(err)); 
 }
+
+const Reminder = mongoose.model("Reminder", reminderSchema);
+module.exports = Reminder; 
